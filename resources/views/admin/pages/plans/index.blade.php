63c4a2dde3da13f2ae68590a2fcb1259234c9957
Luis Augusto Jarillo <luis@neotix.com.br>
2022-06-10 12:55:52 -0300

    Arquivos do adminLTE

---
+++ resources/views/admin/pages/plans/index.blade.php
@@ -0,0 +1,40 @@
+@extends('adminlte::page')
+
+@section('title', 'Planos')
+
+@section('content_header')
+    <h1>Planos</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+
+        </div>
+        <div class="card-body">
+            <table class="table table-condensed">
+                <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Preço</th>
+                    <th style="width: 50px">Ação</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($plans as $plan)
+                <tr>
+                    <td>{{ $plan->name }}</td>
+                    <td>{{ $plan->price }}</td>
+                    <td>
+                        <a href="" class="btn btn-warning">Ver</a>
+                    </td>
+                </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="card-footer">
+            {!! $plans->links() !!}
+        </div>
+    </div>
+@stop
